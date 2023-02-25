199dc3c99cc91d99f4e380f9fce595ae962524f2
timopix <57803428+timopix@users.noreply.github.com>
2023-02-24 18:11:48 -0700

    feat: pass 100 tests

--- src/guessing-game.js
+++ src/guessing-game.js
@@ -1,21 +1,23 @@
 class GuessingGame {
-    constructor() {}
-
-    setRange(min, max) {
-
+    constructor() {
+        this.min = null;
+        this.max = null;
+        this.currentGuess = null;
+      }
+    
+      setRange(min, max) {
+        this.min = min;
+        this.max = max;
     }
 
     guess() {
-
+        this.answer = Math.round((this.max - this.min) / 2) + this.min;
+        return this.answer;
     }
 
-    lower() {
+    lower() { this.max = this.answer; }
 
-    }
-
-    greater() {
-
-    }
+    greater() { this.min = this.answer; }
 }
 
 module.exports = GuessingGame;
